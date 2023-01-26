e729d2891ee54d1a6364aa5eade9ff024a5418a7
emelie8 <emelie_johansen@hotmail.com>
2023-01-26 12:09:37 +0100

    created details component

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 // import App from "./Components/App/App";
 import BannerImg from "./Components/BannerImg/BannerImg";
+import { Details } from "./Components/Details/details";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BannerImg />
+    <Details />
   </React.StrictMode>
 );
