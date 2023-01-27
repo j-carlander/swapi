eed22efb4cf12105ff5eb1e0ebe7141d139c1e66
emelie8 <emelie_johansen@hotmail.com>
2023-01-27 09:17:40 +0100

    created container

---
+++ src/Components/Container/container.js
@@ -0,0 +1,10 @@
+import React from "react";
+// import { Children } from "react";
+
+export function Container({children}) {
+    return (
+        <div>
+            {children}
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 // import App from "./Components/App/App";
 import BannerImg from "./Components/BannerImg/BannerImg";
+import { Container } from "./Components/Container/container";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BannerImg />
+    <Container >
+      <h2>Hej</h2>
+    </Container>
   </React.StrictMode>
 );
