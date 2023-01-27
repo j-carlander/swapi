f6fe2de8f95b89cb0142ca093a039bd73a51d5c9
emelie8 <emelie_johansen@hotmail.com>
2023-01-27 16:03:23 +0100

    moved page rending to app.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,18 +1,39 @@
-
 import './App.css';
+import React from 'react';
+import BannerImg from "../BannerImg/BannerImg";
+import CarouselMenu from "../Carousel/Carousel";
+import { Container } from "../Container/container";
+import { Details } from "../Details/details";
+import { Subtitle } from "../Subtitle/subtitle";
+import { ResultList } from "../resultList/resultList";
+import { FlexContainer } from "../FlexContainer/flexContainer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+async function fetchAPI(url) {
+  let result = await fetch(url)
+  // console.log(result);
+  return await result.json();
+}
+console.log(fetchAPI("https://swapi.dev/api/people/"));
 
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+      <BannerImg />
+      <CarouselMenu />
+      <FlexContainer>
+      <Container>
+        <Subtitle title="Characters"/>
+        <ResultList />
+      </Container>
+      <Container >
+      <Subtitle title="Details" />
+        <Details />
+      </Container>
+      </FlexContainer>
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,30 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-
-// import App from "./Components/App/App";
-import BannerImg from "./Components/BannerImg/BannerImg";
-import CarouselMenu from "./Components/Carousel/Carousel";
-import { Container } from "./Components/Container/container";
-import { Details } from "./Components/Details/details";
-import { Subtitle } from "./Components/Subtitle/subtitle";
-import { ResultList } from "./Components/resultList/resultList";
-import { FlexContainer } from "./Components/FlexContainer/flexContainer";
+import App from "./Components/App/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BannerImg />
-    <CarouselMenu />
-    <FlexContainer>
-    <Container>
-      <Subtitle title="Characters"/>
-      <ResultList />
-    </Container>
-    <Container >
-    <Subtitle title="Details" />
-      <Details />
-    </Container>
-    </FlexContainer>
+    <App/>
   </React.StrictMode>
 );
