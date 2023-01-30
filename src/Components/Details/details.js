301ca8c4cf38f7ab771b32eaac68ed0fb4d2c582
j-carlander <johannes.carlander@gmail.com>
2023-01-30 11:16:39 +0100

    small changes

--- src/Components/Details/details.js
+++ src/Components/Details/details.js
@@ -1,18 +1,41 @@
 import React from "react";
 
-export function Details() {
-    return (
-        <div>
-            <h3>Luke Skywalker</h3>
-            <ul>
-                <li>Height: 172cm</li>
+const peopleDetails = [
+  "height",
+  "mass",
+  "birth_year",
+  "hair_color",
+  "eye_color",
+  "skin_color",
+  "gender",
+];
+
+export function Details(props) {
+  function formatText(string) {
+    string = string.charAt(0).toUpperCase() + string.slice(1);
+    string = string.replaceAll("_", " ");
+    return string;
+  }
+
+  return (
+    <div>
+      <h3>{props.name}</h3>
+      <ul>
+        {/*  {people.map((detail) => {
+          return (
+            <li>
+              {detail}: {props.result.detail}
+            </li>
+          );
+        })}
+        <li>Height: 172cm</li>
                 <li>Mass: 77kg</li>
                 <li>Hair color: Blonde</li>
                 <li>Skin color: Fair</li>
                 <li>Eye color: Blue</li>
                 <li>Birth year: 19BBY</li>
-                <li>Gender: Male</li>
-            </ul>
-        </div>
-    )
+                <li>Gender: Male</li> */}
+      </ul>
+    </div>
+  );
 }
