c1fdc467f396a25e8d54aeb819f7720db782125f
RobbanBroberg <lafarchetamustatche@outlook.com>
2023-01-31 11:45:09 +0100

    slider kinda working

--- src/Components/Carousel/Carousel.js
+++ src/Components/Carousel/Carousel.js
@@ -1,49 +1,30 @@
 import React from "react";
 import "./Carousel.css";
-import { CarouselItem } from "../CarouselItem/carouselItem";
 
-function Carousel({ categories, onCategorySlideBtn }) {
+function Carousel({ categories, onCategorySlideBtn, img, name }) {
 
-  let currentSlide = 0;
-  let nextSlide = 1;
-  let maxSlides = categories.length;
-  
-  console.log('carousel current=' + currentSlide);
-  console.log('carousel next =' + nextSlide);
-  console.log('maxslides=' + maxSlides)
-
-  // fix disabled class;
   return (
     <div className="carousel">
       <div>
        
         <button 
-          className={
-            currentSlide !== 0 ? "left-carousel-btn" : "left-carousel-btn disabled"
-          }
-          data-url={categories[currentSlide].url}
+          className="left-carousel-btn"
+          data-url={categories.url}
           data-value="prev"
           onClick={onCategorySlideBtn}
           >&#5130;</button>
       </div>
       <div className="slide">
-        {categories.map((category) => {
-          return (
-            <CarouselItem
-              key={category.name}
-              img={category.img}
-              name={category.name}
-              url={category.url}
-            />
-          );
-        })}
+        <figure>
+          <img src={img} alt="" />
+          <figcaption>{name}</figcaption>
+        </figure>
+        
       </div>
       <div>
         <button 
-          className={
-            nextSlide !== maxSlides ? "right-carousel-btn" : "right-carousel-btn disabled"
-          }
-          data-url={categories[nextSlide].url}
+          className="right-carousel-btn"
+          data-url={categories.url}
           data-value="next"
           onClick={onCategorySlideBtn}
           >&#5125;</button>
