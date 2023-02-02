32eea65cb06c95320810b5b4abea8d564db04ea7
RobbanBroberg <lafarchetamustatche@outlook.com>
2023-02-03 00:06:37 +0100

    laserprint done, metallic background added and style changes to text done

--- src/Components/resultList/resultList.js
+++ src/Components/resultList/resultList.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, {useRef} from "react";
 import { ResultItem } from "../ResultItem/resultItem";
 import "./resultList.css";
 import lightsaberCross from "../../img/loadingSabersGIF.gif";
+import laserPrinterGIF from "../../img/FASTERlaserPrinterTransparentGIF.gif"
+import printingGIF from "../../img/printingTransparentGIF.gif"
+
 
 export function ResultList(props) {
+
+  const imgRef = useRef(null);
+
   // if the resultlist is empty show loading screen
   if (!props.result)
     return (
@@ -17,15 +23,21 @@ export function ResultList(props) {
   // variables for the next and previous page links from the fetchData result
   let nextPage = props.result.next;
   let prevPage = props.result.previous;
+  
 
   // calculate the total number of pages from the count value of the fetchData result
   let count = Math.ceil(props.result.count / 10);
 
   // the list of results from the fetchData result
   let resultList = props.result.results;
+  
+
+  setTimeout(() => {
+    imgRef.current.style.cssText = "animation: shorten 5000ms steps(80) forwards"
+  }, "1000");
 
   return (
-    <div>
+    <div className="resultList-container">
       <div className="flex-column">
         {resultList.map((item) => {
           return (
@@ -39,6 +51,12 @@ export function ResultList(props) {
           );
         })}
       </div>
+      <div className="parent-container">
+        <div className="printing-container">
+          <img className="laser_printer-gif" src={laserPrinterGIF} alt="gif of laser" />
+          <img ref={imgRef} className="printing-gif" src={printingGIF} alt="gif of laser"/>
+        </div>
+      </div>
       <div className="flex-space-around">
         <button
           className={
@@ -48,10 +66,10 @@ export function ResultList(props) {
           data-url={prevPage}
           onClick={props.onChangePageBtn}>
           &#171;
-        </button>
-        <p>
-          page: {props.pageNumber}/{count}
-        </p>
+          </button>
+          <p className="page-number">
+            Page: {props.pageNumber}/{count}
+          </p>
         <button
           className={
             nextPage ? "right-resultList-btn" : "right-resultList-btn disabled"
